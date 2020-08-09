c76479f278bfbf5148024ae5b84d969413075a35
阿郑zzz <zlh1992@126.com>
2020-08-09 23:38:32 +0800

    20200809

--- array_40_getLeastNumbers.py
+++ array_40_getLeastNumbers.py
@@ -137,6 +137,82 @@ class Solution2:
         quicksort(arr, 0, len(arr) - 1, k)
         return arr[:k]
 
+
+class Solution3:
+    def getLeastNumbers(self, arr, k):
+        arr = self.qsort(arr)
+        return arr[:k]
+    def qsort(self, arr):
+        if not len(arr):
+            return []
+        else:
+        # 在这里以第一个元素为基准线
+            pivot = arr[0]
+            left = self.qsort([x for x in arr[1:] if x < pivot])
+            right = self.qsort([x for x in arr[1:] if x >= pivot])
+        return left+[pivot]+right
+
+class Solution4:
+    def getLeastNumbers(self, arr, k):
+        arr = self.qsort_(arr)
+        return arr[:k]
+
+    def qsort_(self, arr):
+        '''''
+       模拟栈操作实现非递归的快速排序
+       '''
+        if len(arr) < 2:
+            return arr
+        stack = []
+        stack.append(len(arr) - 1)  # 队尾入栈
+        stack.append(0)  # 队首入栈
+        while stack:
+            # 第二次迭代中等于取出上一次分区中的右分区的队尾队首index进行再次partition操作
+            # 然后先对右边的分区不断迭代做递归存index 到stack里 弹出 直到右边分区顺序
+            # 然后开始弹出左边分区 不断迭代做递归 直到左边分区顺序正常
+            l = stack.pop()  # 当前栈中的 分区队首出栈
+            r = stack.pop()  # 分区队尾出栈
+            index = self.partition(arr, l, r)  # 返回 分区后的数组 比基准值小的在左边，+基准值+右边
+            if l < index - 1:
+                stack.append(index - 1)  # 左分区队尾入栈
+                stack.append(l)  # 左分区队首入栈
+            if r > index + 1:
+                stack.append(r)  # 右分区队尾入栈
+                stack.append(index + 1)  # # 右分区队首入栈
+        return arr
+
+    def partition(self, li, low, high):
+        # 这个函数是用来找出确定值的索引
+        # 首先设置俩个布尔变量，通过这个来控制左右移动
+        high_flag = True
+        low_flag = False
+        # 将开始位置的值定为基数
+        pivot = li[low]
+        while low < high and low < len(li) and high < len(li):
+            # 当这个值为真时，游标从右开始移动
+            if high_flag:
+                # 找出右边比基数小的值，互换位置，否则一直向右移动
+                if li[high] < pivot:
+                    # print('before high2low',li)
+                    li[low] = li[high]
+                    # 改变布尔值，控制方向
+                    high_flag = False
+                    low_flag = True
+                    # print('after high2low',li)
+                else:
+                    high -= 1
+            if low_flag:
+                if li[low] > pivot:
+                    # print('before low2high',li)
+                    li[high] = li[low]
+                    high_flag = True
+                    low_flag = False
+                    # print('after low2high',li)
+                else:
+                    low += 1
+        li[low] = pivot  # 基准值放如list中间位置
+        # 返回的是索引位置 单list顺序已经被分为了左队列 基准值 右队列
+        return low
 if __name__ == "__main__":
     sl = Solution()
     ans = sl.getLeastNumbers([1, 4, 5, 1, 3, 5, 2], 3)
