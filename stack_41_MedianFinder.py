c321b3ae392c5df8938165ac8574503ec13682c4
阿郑zzz <zlh1992@126.com>
2020-08-10 23:43:32 +0800

    20200810

--- stack_41_MedianFinder.py
+++ stack_41_MedianFinder.py
@@ -26,10 +26,11 @@ pd.set_option('max_colwidth', 200)
 pd.set_option('display.width', 5000)
 
 
-class MedianFinder(object):
+class MedianFinder1(object):
     '''
     通过二分查找维护保序队列执行用时： 2416 ms  内存消耗：24.9 MB
     '''
+
     def __init__(self):
         """
         initialize your data structure here.
@@ -84,12 +85,76 @@ class MedianFinder(object):
             return (self.arr[n // 2] + self.arr[n // 2 - 1]) / 2
 
 
+from heapq import *
+
+class MedianFinder(object):
+    '''
+    使用大根堆和小根堆
+    假想我们现在有两个容器 A, B 这两个容器将我们的整体数据分成两部分,且 A 中的数据都小于 B 中的数据,并且 A 中的最后一个数据是 A 里面最大的B 的第一个数据是 B 中最小的.
+    好了 我们有了上面的条件,那我们怎么找到中位数呢?
+    当整体数目为奇数时,中间的那个数就是所求.当整体数目为偶数时,中间两个数的和再除以 2 ,就能得到结果
+    我们只要将上面的两个容器的数据数目只差保持在 1 之内即可,也就是说 A 和 B 将整体数据以中位数划分开来了
+    '''
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.bigHpeap = []
+        self.smallHeap = []
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        if len(self.bigHpeap) == len(self.smallHeap):  # 总数为偶数时,先插入到大根堆,在插入到小根堆
+            heappush(self.smallHeap, -heappushpop(self.bigHpeap, -num))
+        else:  # 总数为奇数时,先插入到小根堆,在插入到大根堆
+            heappush(self.bigHpeap, -heappushpop(self.smallHeap, num))
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        if len(self.bigHpeap) == len(self.smallHeap):
+            return (-self.bigHpeap[0] + self.smallHeap[0]) / 2.0
+        else:
+            return self.smallHeap[0]
+
+# heapq:
+'''
+
+heapq 模块提供了堆算法。heapq是一种子节点和父节点排序的树形数据结构。
+这个模块提供heap[k] <= heap[2*k+1] and heap[k] <= heap[2*k+2]。为了比较不存在的元素被人为是无限大的。heap最小的元素总是[0]
+
+方法heappush
+heap = []
+heapq.heappush(heap, i)
+根据结果可以了解，子节点的元素大于父节点元素。而兄弟节点则不会排序。
+
+方法heapq.heapify(list)
+将list类型转化为heap, 在线性时间内, 重新排列列表
+
+方法删除并返回堆中最小的元素, 通过heapify() 和heappop()来排序
+
+方法heapq.heapreplace(iterable, n)
+删除现有元素并将其替换为一个新值
+
+方法heapq.nlargest(n, iterable) 和 heapq.nsmallest(n, iterable)
+
+'''
+
 if __name__ == "__main__":
-    mf = MedianFinder()
+    mf = MedianFinder1()
     arr = [1, 4, 5, 9, 13, 14, 15, 19, 20]
-    # item = 7
-    # index = mf.binary_search(arr,item)
     for i in arr[::-1]:
         mf.addNum(i)
     print(mf.arr)
+    print(mf.findMedian())
+
+    mf = MedianFinder()
+    arr = [1, 4, 5, 9, 13, 14, 15, 19, 20]
+    for i in arr[::-1]:
+        mf.addNum(i)
+    print(mf.smallHeap, mf.bigHpeap)
     print(mf.findMedian())
