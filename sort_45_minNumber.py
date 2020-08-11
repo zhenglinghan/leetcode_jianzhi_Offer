85d9170ca68c36520df1aaa8e849b0140ed02ee4
阿郑zzz <zlh1992@126.com>
2020-08-11 21:20:11 +0800

    20200811

---
+++ sort_45_minNumber.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: sort_45_minNumber.py
+@time: 2020/8/11 21:20
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+def func():
+    pass
+
+
+class Main:
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass 
