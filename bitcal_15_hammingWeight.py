028b261bf8d890f66da4490db5a14461392d5743
阿郑zzz <zlh1992@126.com>
2020-08-12 16:28:06 +0800

    20200812

---
+++ bitcal_15_hammingWeight.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: bitcal_15_hammingWeight.py
+@time: 2020/8/12 16:26
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+class Solution(object):
+    def hammingWeight1(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
+        return res
+
+    def hammingWeight2(self, n):
+        res = 0
+        while n:
+            res += 1
+            n &= n - 1
+        return res
+
+
+if __name__ == "__main__":
+    a = 23
+    sl = Solution()
+    ans1 = sl.hammingWeight1(a)
+    ans2 = sl.hammingWeight2(a)
+    print(ans1, ans2)
