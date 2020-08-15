6ac0db65b750e4b426f6662fc46ea09ac84598d7
阿郑zzz <zlh1992@126.com>
2020-08-15 10:36:31 +0800

    20200814

--- tree_28_isSymmetric.py
+++ tree_28_isSymmetric.py
@@ -42,10 +42,34 @@ class Solution(object):
                 return False
             return L.val == R.val and recur(L.left, R.right) and recur(L.right, R.left)
 
-        return recur(root.left, root.right) if root else True
+        return recur(root.left, root.right) if root else True # else True 是空树对称的题目要求
+
 
 '''
 对比子树结构的问题
+
+对称二叉树，树里每个节点下的左右子树都有：
+对称二叉树定义： 对于树中 任意两个对称节点 L 和 R ，一定有：
+L.val = R.val ：即此两对称节点值相等。
+L.left.val = R.right.val ：即 L 的 左子节点 和 R 的 右子节点 对称；
+L.right.val = R.left.val ：即 L 的 右子节点 和 R 的 左子节点 对称。
+根据以上规律，考虑从顶至底递归，判断每对节点是否对称，从而判断树是否为对称二叉树 一般都递归到叶节点结束
+
+算法流程：
+isSymmetric(root) ：
+
+特例处理： 若根节点 root 为空，则直接返回 true 。
+返回值： 即 recur(root.left, root.right) ;
+recur(L, R) ：
+
+终止条件：
+当 L 和 R 同时越过叶节点： 此树从顶至底的节点都对称，因此返回 true ；
+当 L 或 R 中只有一个越过叶节点： 此树不对称，因此返回 false ；
+当节点 L 值 =/= 节点 R 值： 此树不对称，因此返回 false ；
+递推工作：
+判断两节点 L.left 和 R.right 是否对称，即 recur(L.left, R.right) ；
+判断两节点 L.right 和 R.left 是否对称，即 recur(L.right, R.left) ；
+返回值： 两对节点都对称时，才是对称树，因此用与逻辑符 && 连接。
 
 '''
 
