56163198162d3fb1939b78a1f0fb49d011bdae3d
阿郑zzz <zlh1992@126.com>
2020-08-17 23:57:02 +0800

    20200817

---
+++ tree_37_serialize_deserialize.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: tree_37_serialize_deserialize.py
+@time: 2020/8/17 22:52
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return []
+        stack = []
+        res = []
+        stack.append(root)
+        while stack:
+            nodei = stack.pop(0)
+            if nodei:
+                res.append(nodei.val)
+                stack.append(nodei.left)
+                stack.append(nodei.right)
+            else:
+                res.append("null")
+        return res
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
+
+
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.right.left = TreeNode(4)
+    root.right.right = TreeNode(5)
+    codec = Codec()
+    ans1 = codec.serialize(root)
