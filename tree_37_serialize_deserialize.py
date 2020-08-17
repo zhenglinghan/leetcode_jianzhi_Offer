8e0165309882e2645859a4e85c22cec9a45169e0
阿郑zzz <zlh1992@126.com>
2020-08-17 23:57:14 +0800

    20200817

--- tree_37_serialize_deserialize.py
+++ tree_37_serialize_deserialize.py
@@ -33,35 +33,72 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+# class Codec:
+#
+#     def serialize(self, root):
+#         """Encodes a tree to a single string.
+#
+#         :type root: TreeNode
+#         :rtype: str
+#         """
+#         if not root:
+#             return []
+#         stack = []
+#         res = []
+#         stack.append(root)
+#         while stack:
+#             nodei = stack.pop(0)
+#             if nodei:
+#                 res.append(nodei.val)
+#                 stack.append(nodei.left)
+#                 stack.append(nodei.right)
+#             else:
+#                 res.append("null")
+#         return res
+#
+#     def deserialize(self, data):
+#         """Decodes your encoded data to tree.
+#
+#         :type data: str
+#         :rtype: TreeNode
+#         """
+
+
 class Codec:
 
     def serialize(self, root):
-        """Encodes a tree to a single string.
-
-        :type root: TreeNode
-        :rtype: str
-        """
-        if not root:
-            return []
-        stack = []
+        if not root: return "[]"
+        queue = collections.deque()
+        queue.append(root)
         res = []
-        stack.append(root)
-        while stack:
-            nodei = stack.pop(0)
-            if nodei:
-                res.append(nodei.val)
-                stack.append(nodei.left)
-                stack.append(nodei.right)
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
             else:
                 res.append("null")
-        return res
+        return '[' + ','.join(res) + ']'
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-
-        :type data: str
-        :rtype: TreeNode
-        """
+        if data == "[]":
+            return
+        vals, i = data[1:-1].split(','), 1
+        root = TreeNode(int(vals[0]))
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            if vals[i] != "null":
+                node.left = TreeNode(int(vals[i]))
+                queue.append(node.left)
+            i += 1
+            if vals[i] != "null":
+                node.right = TreeNode(int(vals[i]))
+                queue.append(node.right)
+            i += 1
+        return root
 
 
 # Your Codec object will be instantiated and called as such:
