99068665b7ea6907d4f122414eb4e03451e3ef31
阿郑zzz <zlh1992@126.com>
2020-08-19 00:19:57 +0800

    20200819

---
+++ tree_68_lowestCommonAncestor.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: tree_68_lowestCommonAncestor.py
+@time: 2020/8/18 23:36
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+'''
+方法一：迭代
+循环搜索： 当节点 rootroot 为空时跳出；
+当 p, qp,q 都在 rootroot 的 右子树 中，则遍历至 root.rightroot.right ；
+否则，当 p, qp,q 都在 rootroot 的 左子树 中，则遍历至 root.leftroot.left ；
+否则，说明找到了 最近公共祖先 ，跳出。
+返回值： 最近公共祖先 rootroot 。
+
+方法二：递归
+递推工作：
+当 p, qp,q 都在 rootroot 的 右子树 中，则开启递归 root.rightroot.right 并返回；
+否则，当 p, qp,q 都在 rootroot 的 左子树 中，则开启递归 root.leftroot.left 并返回；
+返回值： 最近公共祖先 rootroot 。
+'''
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    '''
+    循环解法
+    '''
+
+    def lowestCommonAncestor(self, root, p, q):
+        if p.val > q.val: p, q = q, p  # 保证 p.val < q.val # 优化条件判断
+        while root:
+            if root.val < p.val:  # p,q 都在 root 的右子树中
+                root = root.right  # 遍历至右子节点
+            elif root.val > q.val:  # p,q 都在 root 的左子树中
+                root = root.left  # 遍历至左子节点
+            else:
+                break
+        return root
+
+    '''
+    递归解法
+    '''
+
+    def lowestCommonAncestor_2(self, root, p, q):
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
+
+
+if __name__ == "__main__":
+    pass
---
+++ tree_68_lowestCommonAncestorII.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: tree_68_lowestCommonAncestorII.py
+@time: 2020/8/18 23:47
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+'''
+递归解析：
+终止条件：
+当越过叶节点，则直接返回 nullnull ；
+当 rootroot 等于 p, qp,q ，则直接返回 rootroot ；
+递推工作：
+开启递归左子节点，返回值记为 leftleft ；
+开启递归右子节点，返回值记为 rightright ；
+返回值： 根据 leftleft 和 rightright ，可展开为四种情况；
+当 leftleft 和 rightright 同时为空 ：说明 rootroot 的左 / 右子树中都不包含 p,qp,q ，返回 nullnull ；
+当 leftleft 和 rightright 同时不为空 ：说明 p, qp,q 分列在 rootroot 的 异侧 （分别在 左 / 右子树），因此 rootroot 为最近公共祖先，返回 rootroot ；
+当 leftleft 为空 ，rightright 不为空 ：p,qp,q 都不在 rootroot 的左子树中，直接返回 rightright 。具体可分为两种情况：
+p,qp,q 其中一个在 rootroot 的 右子树 中，此时 rightright 指向 pp（假设为 pp ）；
+p,qp,q 两节点都在 rootroot 的 右子树 中，此时的 rightright 指向 最近公共祖先节点 ；
+当 leftleft 不为空 ， rightright 为空 ：与情况 3. 同理；
+观察发现， 情况 1. 可合并至 3. 和 4. 内
+'''
+'''
+考虑通过递归对二叉树进行后序遍历，当遇到节点 pp 或 qq 时返回。从底至顶回溯，当节点 p, qp,q 在节点 rootroot 的异侧时，节点 rootroot 即为最近公共祖先，则向上返回 rootroot 。
+'''
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left and not right: return # 1.
+        if not left: return right # 3.
+        if not right: return left # 4.
+        return root # 2. if left and right:
+
+
+if __name__ == "__main__":
+    pass
