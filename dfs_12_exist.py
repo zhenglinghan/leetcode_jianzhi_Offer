b09fcf22580d3d83cec6cb468c3e735d9889cf12
阿郑zzz <zlh1992@126.com>
2020-08-21 23:54:52 +0800

    2020081 DFS 难以理解啊

---
+++ dfs_12_exist.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: dfs_12_exist.py
+@time: 2020/8/21 23:53
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+class Solution:
+    def exist(self, board, word):
+        def dfs(i, j, k):
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]:
+                return False
+            if k == len(word) - 1:
+                return True
+            tmp, board[i][j] = board[i][j], '/'
+            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            board[i][j] = tmp
+            return res
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0):
+                    return True
+        return False
+
+
+if __name__ == "__main__":
+    pass
