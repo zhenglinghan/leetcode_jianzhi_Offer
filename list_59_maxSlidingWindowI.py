a7dfef977a7fab299f9188f3661084a75607ca4f
阿郑zzz <zlh1992@126.com>
2020-08-24 23:59:24 +0800

    20200824 先进先出队列

--- list_59_maxSlidingWindowI.py
+++ list_59_maxSlidingWindowI.py
@@ -39,7 +39,18 @@ class Solution:
             res.append(max(nums[:k]))
         return res
 
-
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        deque = collections.deque()
+        res, n = [], len(nums)
+        for i, j in zip(range(1 - k, n + 1 - k), range(n)):
+            if i > 0 and deque[0] == nums[i - 1]:
+                deque.popleft()  # 删除 deque 中对应的 nums[i-1]
+            while deque and deque[-1] < nums[j]:
+                deque.pop()  # 保持 deque 递减
+            deque.append(nums[j])
+            if i >= 0:
+                res.append(deque[0])  # 记录窗口最大值
+        return res
 if __name__ == "__main__":
     s = Solution()
     nums = [1, 3, -1, -3, 5, 3, 6, 7]
