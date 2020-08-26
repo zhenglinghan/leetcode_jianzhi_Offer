bc7112b312f66f456563c73effe6561232096dca
阿郑zzz <zlh1992@126.com>
2020-08-26 23:39:30 +0800

    20200826

---
+++ dp_14_cuttingRopeII.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: dp_14_cuttingRopeII.py
+@time: 2020/8/26 23:39
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+class Solution:
+    def cuttingRope(self, n: int) -> int:
+        dp = [0 for _ in range(n + 1)]  # dp[0] dp[1]其实没用
+        dp[2] = 1  # 初始化
+        res = -1
+        for i in range(3, n + 1):
+            for j in range(i):
+                dp[i] = max(dp[i], max((i - j) * j, j * dp[i - j]))
+        return dp[n] % 1000000007
+
+
+if __name__ == "__main__":
+    pass
