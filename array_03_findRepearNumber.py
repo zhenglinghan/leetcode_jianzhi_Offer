0aa04c6821f589b34b20be8cb4d2f21105b1d349
阿郑zzz <zlh1992@126.com>
2020-08-31 23:31:48 +0800

    20200831

---
+++ array_03_findRepearNumber.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: array_03_findRepearNumber.py
+@time: 2020/8/31 23:31
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+class Solution:
+    '''
+    维护hash表
+    '''
+
+    def findRepeatNumber(self, nums):
+        d = set()
+        for i in nums:
+            if i not in d:
+                d.add(i)
+            else:
+                return i
+        return None
+
+    '''
+    索引查重 原地交换
+    '''
+    def findRepeatNumber(self, nums):
+        i = 0
+        while i<len(nums):
+            if nums[i]==i:
+                i+=1
+                continue
+            if nums[nums[i]]==nums[i]:
+                return nums[i]
+            else:
+                nums[nums[i]],nums[i] = nums[i],nums[nums[i]]
+
+if __name__ == "__main__":
+    pass 
