3b6ce8e917d8ee28775d1feb4f52a931b06b932b
阿郑zzz <zlh1992@126.com>
2020-09-01 00:06:51 +0800

    20200831

--- array_04_findNumberIn2DArray.py
+++ array_04_findNumberIn2DArray.py
@@ -26,13 +26,40 @@ pd.set_option('max_colwidth', 200)
 pd.set_option('display.width', 5000)
 
 
-def func():
-    pass
+class Solution:
+    def binary_search(self, matrix, target, start, vertical):
+        lo = start
+        hi = len(matrix) - 1 if vertical else len(matrix[0]) - 1 # 垂直搜索：hi = 行数 - 1
 
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if vertical:  # 垂直搜索
+                if matrix[mid][start] < target:
+                    lo = mid + 1
+                elif matrix[mid][start] > target:
+                    hi = mid - 1
+                else:
+                    return True
+            else:   # 水平搜索
+                if matrix[start][mid] < target:
+                    lo = mid + 1
+                elif matrix[start][mid] > target:
+                    hi = mid - 1
+                else:
+                    return True
 
-class Main:
-    def __init__(self):
-        pass
+        return False
+
+    def findNumberIn2DArray(self, matrix, target):
+        if not matrix: return False   # 边界条件
+
+        for i in range(min(len(matrix), len(matrix[0]))):
+            vertical_found = self.binary_search(matrix, target, i, True) # 垂直方向是否找到
+            horizontal_found = self.binary_search(matrix, target, i, False) # 水平是否找到
+            if vertical_found or horizontal_found:  # 任一方向找到即可
+                return True
+
+        return False
 
 
 if __name__ == "__main__":
