985b087628c3f052e002e63a439d9be256a3a4f6
阿郑zzz <zlh1992@126.com>
2020-09-01 23:59:50 +0800

    20200901

---
+++ array_29_spiralOrder.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: array_29_spiralOrder.py
+@time: 2020/9/1 23:56
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+''' 
+语法糖！
+'''
+
+
+def spiralOrder(matrix):
+    res = []
+    while matrix:
+        res += matrix.pop(0)
+        matrix = list(zip(*matrix))[::-1]
+    return res
+
+
+class Solution:
+    def spiralOrder(self, matrix):
+        if not matrix: return []
+        l, r, t, b, res = 0, len(matrix[0]) - 1, 0, len(matrix) - 1, []
+        while True:
+            for i in range(l, r + 1): res.append(matrix[t][i])  # left to right
+            t += 1
+            if t > b: break
+            for i in range(t, b + 1): res.append(matrix[i][r])  # top to bottom
+            r -= 1
+            if l > r: break
+            for i in range(r, l - 1, -1): res.append(matrix[b][i])  # right to left
+            b -= 1
+            if t > b: break
+            for i in range(b, t - 1, -1): res.append(matrix[i][l])  # bottom to top
+            l += 1
+            if l > r: break
+        return res
+
+
+if __name__ == "__main__":
+    pass
