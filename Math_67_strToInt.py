309e439cc365983ef7bb004c1f3ee32e49f32981
阿郑zzz <zlh1992@126.com>
2020-09-13 00:15:24 +0800

    20200912 Math

--- Math_67_strToInt.py
+++ Math_67_strToInt.py
@@ -29,8 +29,16 @@ pd.set_option('display.width', 5000)
 class Solution:
     def strToInt(self, str):
         res, i, sign, length = 0, 0, 1, len(str)  # 结果数字 索引 符号 长度
-        int_max, int_min, bndry = 2 ** 31 - 1, -2 ** 31, 2 ** 31 // 10
+        int_max, int_min, bndry = 2 ** 31 - 1, -2 ** 31, 2 ** 31 // 10# bndry是214748364
         '''
+        数字字符：
+        字符转数字： “此数字的 ASCII 码” 与 “ 0 的 ASCII 码” 相减即可；
+        数字拼接： 若从左向右遍历数字，设当前位字符为 c ，当前位数字为 x，数字结果为 res ，则数字拼接公式为：
+        res = 10 * res + x
+        x = ascii(c) - ascii('0')
+        res=10×res+x
+        x=ascii(c)−ascii('0')
+
         在每轮数字拼接前，判断 res 在此轮拼接后是否超过 2147483647 ，若超过则加上符号位直接返回。
         设数字拼接边界 bndry = 2147483647 // 10 = 214748364，则以下两种情况越界：
 
@@ -41,17 +49,22 @@ class Solution:
         if not str:
             return 0  # 空字符串，提前返回
         while str[i] == ' ':
+            # 一开始从i开始计数，i之前都为' ' 去除
             i += 1
             if i == length:
                 return 0  # 字符串全为空格，提前返回
+        # 记录符号
         if str[i] == '-':
             sign = -1
         if str[i] in '+-':
             i += 1
         for c in str[i:]:
-            if not '0' <= c <= '9': break
-            if res > bndry or res == bndry and c > '7':
+            if not '0' <= c <= '9':
+                break# 异常！直接返回
+            if res > bndry or (res == bndry and c > '7'):# 优先级 and 高 其实不用括号
+                # 越界
                 return int_max if sign == 1 else int_min
+            # ord() 函数是 chr() 函数的配对函数 返回一个字符的ascii码
             res = 10 * res + ord(c) - ord('0')
         return sign * res
 
