0f19229cc7a0f88854005e9de270f14d1bfbac9f
阿郑zzz <zlh1992@126.com>
2020-09-22 23:59:49 +0800

    20200922 双指针

---
+++ array_21_exchange.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: array_21_exchange.py
+@time: 2020/9/22 23:58
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+
+class Solution:
+    '''
+    wc 双指针 保证i左边都是奇数 i右边都是偶数
+    '''
+    def exchange(self, nums):
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            while i < j and nums[i] & 1 == 1:
+                i += 1
+            while i < j and nums[j] & 1 == 0:
+                j -= 1
+            i += 1
+            j -= 1
+        return nums
+
+
+if __name__ == "__main__":
+    pass
