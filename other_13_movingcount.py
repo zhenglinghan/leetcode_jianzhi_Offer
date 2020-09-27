50713847e224699921150b9fb290a5f4bec58f8c
阿郑zzz <zlh1992@126.com>
2020-09-28 00:03:58 +0800

    20200927 回溯！

---
+++ other_13_movingcount.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@version: v1.0
+@author: zhenglinghan
+@contact: 422807471@qq.com
+@software: PyCharm
+@file: other_13_movingcount.py
+@time: 2020/9/27 23:59
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import gc
+import datetime as dt
+import warnings
+
+warnings.filterwarnings("ignore")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('precision', 5)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+pd.set_option('max_colwidth', 200)
+pd.set_option('display.width', 5000)
+
+'''
+矩阵中的搜索与回溯！
+'''
+class Solution:
+    '''
+    dfs
+    '''
+
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        def dfs(i, j, si, sj):
+            if i >= m or j >= n or k < si + sj or (i, j) in visited: return 0
+            visited.add((i, j))
+            # 1 + 右方搜索的可达解总数 + 下方搜索的可达解总数
+            return 1 + dfs(i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj) + dfs(i, j + 1, si,
+                                                                                   sj + 1 if (j + 1) % 10 else sj - 8)
+
+        visited = set()
+        return dfs(0, 0, 0, 0)
+
+
+class Solution:
+    '''
+    bfs
+    '''
+
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        queue, visited, = [(0, 0, 0, 0)], set()
+        while queue:
+            i, j, si, sj = queue.pop(0)
+            if i >= m or j >= n or k < si + sj or (i, j) in visited: continue
+            visited.add((i, j))
+            queue.append((i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj))
+            queue.append((i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8))
+        return len(visited)
